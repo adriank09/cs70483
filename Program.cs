a6a9447951723a611bbdff3b98d7cb7b71b7ba47
Adrian Khor <adriank96@outlook.com>
2019-03-29 21:32:46 +0900

    BlockingCollection demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -28,19 +29,52 @@ namespace core_app
         
         static void Main(string[] args)
         {
+            BlockingCollection<int> data = new BlockingCollection<int>(10);
+            
+            // data up to 10 iterations can be added
+            Task.Run(() => 
+            {
+                for(int i=0;i<20;i++)
+                {
+                    data.Add(i);
+                    System.Console.WriteLine("Data '{0}' added successfully.", i);
+                }
+                data.CompleteAdding();
+            });
+            System.Console.WriteLine("Reading collection...");
+            Task.Run(() => 
+            {
+                while(!data.IsCompleted)
+                {
+                    try
+                    {
+                        int val = data.Take();
+                        System.Console.WriteLine("Data '{0}' taken successfully.", val);
+                    }
+                    catch (System.Exception)
+                    {
+                        
+                        throw;
+                    }
+                }
+            });
+            
+            
+            
+            
             // var webPage = GetWebPageAsync("https://www.newinti.edu.my").Result;
             // System.Console.WriteLine(webPage);
 
-            var multipleWebPages = GetWebPagesAsync(new string[] {
-                "https://www.newinti.edu.my",
-                "https://www.adriankhor.my"
-            }).Result;
+            // var multipleWebPages = GetWebPagesAsync(new string[] {
+            //     "https://www.newinti.edu.my",
+            //     "https://www.adriankhor.my"
+            // }).Result;
 
-            foreach(var result in multipleWebPages)
-            {
-                System.Console.WriteLine(result);
-                System.Console.WriteLine();
-            }
+            // foreach(var result in multipleWebPages)
+            // {
+            //     System.Console.WriteLine(result);
+            //     System.Console.WriteLine();
+            // }
         }
     }
 }
